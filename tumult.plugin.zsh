ca22de93847e01e8831de0d2fd162f5b5092cddc
Joe Block <jpb@unixorn.net>
2016-07-12 14:04:17 -0500

    Remove ls color setup since it isn't OSX-specific

--- tumult.plugin.zsh
+++ tumult.plugin.zsh
@@ -202,10 +202,4 @@ if [[ "$(uname -s)" = "Darwin" ]]; then
     alias postgres-unload="launchctl unload -w /usr/local/Cellar/postgresql/9.4.5/homebrew.mxcl.postgresql.plist"
   fi
 
-  # Yes, these are a pain to customize. Fortunately, Geoff Greer made an online
-  # tool that makes it easy to customize your color scheme and keep them in sync
-  # across Linux and OS X/*BSD at http://geoff.greer.fm/lscolors/
-
-  export LSCOLORS=exfxcxdxbxegedAbAgacad
-  export LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=1;;41:sg=1;;46:tw=0;42:ow=0;43:"
 fi
